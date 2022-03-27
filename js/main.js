0f3e8a0070332acaf8b935634a04da12fb170c28
Martina Tricarico <tricaricomartina4@gmail.com>
2022-03-27 16:31:58 -0300

    finally no se

--- js/main.js
+++ js/main.js
@@ -12,3 +12,13 @@ $(".navbar a").on("click", function (e) {
     );
   }
 });
+
+const $form = document.querySelector("#form");
+
+$form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = new FormData(this);
+  console.log(form.get("name"));
+}
